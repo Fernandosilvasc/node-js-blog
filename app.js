fc025b8967e2ed78607f703f0bb0243cde8d0074
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-09-21 12:06:41 -0700

    setup model, control and view

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const blogRouter = require('./routes/blog');
+const Article = require('./models/article')
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+
+
+app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: false}));
+app.use('/public', express.static('public'));
+app.use(methodOverride('_method'));// we will use to override the method 
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc'})
+    res.render('articles/index', { articles :articles})
+})
+
+app.use('/articles', blogRouter);
+
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log('Connected to Database!');
+        app.listen(PORT);
+    })
+    .catch(err => console.log(err))
